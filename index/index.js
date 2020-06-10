00541e77619517842a75acde7428f2649923ce95
罗建飞 <460918993@qq.com>
2020-06-10 14:57:31 +0800

    页面
    加入一些控件

--- index/index.js
+++ index/index.js
@@ -1,12 +1,41 @@
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 0
-        })
-      }
-    }
+Page({
+ 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pickerHidden: true,
+    chosen: ''
+  },
+  pickerConfirm(e) {
+    this.setData({
+      pickerHidden: true
+    })
+    this.setData({
+      chosen: e.detail.value
+    })
+  },
+
+  pickerCancel() {
+    this.setData({
+      pickerHidden: true
+    })
+  },
+
+  pickerShow() {
+    this.setData({
+      pickerHidden: false
+    })
+  },
+
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
+    })
   }
 })
